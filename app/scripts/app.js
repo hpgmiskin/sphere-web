826bf6685c467efaed207e8d7ad609642c4fc8c2
rick <kingkongi@outlook.com>
2015-07-14 12:06:42 +0100

    Removed default yeoman html + css code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,16 +24,6 @@ angular
         controller: 'IndexCtrl',
         controllerAs: 'index'
       })
-      .when('/main', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
       .otherwise({
         redirectTo: '/'
       });
