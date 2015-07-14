48ff4c4ef7ae65d3eb5514a79f7fd21c40b29f57
rick <kingkongi@outlook.com>
2015-07-14 17:22:41 +0100

    Removed redundant html code, replaced with a ng-repeat tag

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -2,16 +2,47 @@
 
 /**
  * @ngdoc function
- * @name webApp.controller:MainCtrl
+ * @name webApp.controller:IndexCtrl
  * @description
- * # MainCtrl
- * Controller of the webApp
+ * # IndexCtrl
+ * Controller of the index page
  */
 angular.module('webApp')
-  .controller('IndexCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('IndexCtrl', function ($scope) {
+
+    /*List of features*/
+    $scope.featureList = [
+      {
+        "title": "Your Bills, unified",
+        "desc": "Look after your Phone, Electricity, Gas and much \nmore in one simple App.",
+        "img_src": "http://placehold.it/350x150"
+      },
+      {
+        "title": "Analyze Your Bills",
+        "desc": "Analyze your bills over time.\nSpending too much on Electricity?" +
+        "How much heating did you use in winter?",
+        "img_src": "http://placehold.it/350x150"
+      },
+      {
+        "title": "Never miss a payment",
+        "desc": "We'll make sure your kept up to date with all your bills.\n" +
+        "You'll know exactly how long till your next payment.",
+        "img_src": "http://placehold.it/350x150"
+      },
+      {
+        "title": "Split Bills",
+        "desc": "Easily split bills between house mates and receive payments from\n housemates.",
+        "img_src": "http://placehold.it/350x150"
+      },
+      {
+        "title": "Save on Your Bills",
+        "desc": "Automatically compare providers and easily switch to the cheapest \n providers.",
+        "img_src": "http://placehold.it/350x150"
+      },
+      {
+        "title": "Get Accurate Bills",
+        "desc": "Submit meter readings to your provider for more accurate bills",
+        "img_src": "http://placehold.it/350x150"
+      }
     ];
   });
