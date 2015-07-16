8fb80b4b7221302092fc71484301e013a6da6b4c
rick <kingkongi@outlook.com>
2015-07-16 09:56:00 +0100

    Added art assets to index page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,34 +14,34 @@ angular.module('sphereApp')
       {
         "title": "Your Bills, unified",
         "desc": "Look after your Phone, Electricity, Gas and much \nmore in one simple App.",
-        "img_src": "http://placehold.it/350x150"
+        "img_src": "../images/index/BillsIcon@1x.png"
       },
       {
         "title": "Analyze Your Bills",
         "desc": "Analyze your bills over time.\nSpending too much on Electricity?" +
         "How much heating did you use in winter?",
-        "img_src": "http://placehold.it/350x150"
+        "img_src": "../images/index/AnalyzeIcon.png"
       },
       {
         "title": "Never miss a payment",
         "desc": "We'll make sure your kept up to date with all your bills.\n" +
         "You'll know exactly how long till your next payment.",
-        "img_src": "http://placehold.it/350x150"
+        "img_src": "../images/index/NotificationIcon.png"
       },
       {
         "title": "Split Bills",
         "desc": "Easily split bills between house mates and receive payments from\n housemates.",
-        "img_src": "http://placehold.it/350x150"
+        "img_src": "../images/index/Friends.png"
       },
       {
         "title": "Save on Your Bills",
         "desc": "Automatically compare providers and easily switch to the cheapest \n providers.",
-        "img_src": "http://placehold.it/350x150"
+        "img_src": "../images/index/SaveIcon.png"
       },
       {
         "title": "Get Accurate Bills",
         "desc": "Submit meter readings to your provider for more accurate bills",
-        "img_src": "http://placehold.it/350x150"
+        "img_src": "../images/index/Oval_1_Shape.png"
       }
     ];
   });
